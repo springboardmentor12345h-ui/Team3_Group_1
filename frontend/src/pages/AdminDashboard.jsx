abbbc24044c15590510a4badca394b236df89e95
hannasibichan <hannasibichan139@gmail.com>
2026-02-15 09:30:43 +0530

    merged

--- frontend/src/pages/AdminDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function AdminDashboard() {
-  return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
-  );
-}
--- frontend/src/pages/StudentDashboard.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function StudentDashboard() {
-  return (
-    <div>
-      <h1>Student Dashboard</h1>
-    </div>
-  );
-}
