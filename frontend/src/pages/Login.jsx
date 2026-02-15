8b34b923670d1ec09d1ca3da037fe216c86469c6
hannasibichan <hannasibichan139@gmail.com>
2026-02-15 09:46:48 +0530

    temporary

--- frontend/src/pages/Login.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Login() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  );
-}
--- frontend/src/pages/Register.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export default function Register() {
-  return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
-  );
-}
