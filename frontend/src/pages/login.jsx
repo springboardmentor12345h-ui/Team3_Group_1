00e46b50b1f90b45bb113a1ee7c8259e9bffe4f0
hannasibichan <hannasibichan139@gmail.com>
2026-02-21 23:28:10 +0530

    Home page updated

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import "../styles/auth.css";
-import Chatbot from '../components/chatbot';
+import Chatbot from '../components/Chatbot';
 
 export default function Login() {
   const { login } = useContext(AuthContext);
